cf9ebbfc8f28600ec8472fabfff5a5922c4451fd
yongho shin <gdtknight@naver.com>
2023-04-18 15:37:47 +0900

    최종 배포 테스트 커핏 - 1

--- src/main/java/io/github/menzcare/backoffice/BackOfficeApplication.java
+++ src/main/java/io/github/menzcare/backoffice/BackOfficeApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackOfficeApplication {
+  
   public static void main(String[] args) {
     SpringApplication.run(BackOfficeApplication.class, args);
   }
+  
 }
